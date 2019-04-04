8d51897d5d62757748620381d1b4e19ad427d5da
Anwar Abdullah <anwarabdullahn28@gmail.com>
2019-04-05 02:15:42 +0700

    sort top 20 topic by upvote

--- src/components/Topic.js
+++ src/components/Topic.js
@@ -24,31 +24,34 @@ const Topic = (props) => {
 		</Col>
 	);
 
-	const topicList = props.topics.map((topic) => (
-		<Col className="my-3" lg={3} md={3} sm={6} key={topic.id}>
-			<Card style={{ height: '210px' }}>
-				<CardBody>
-					<h4 className="text-center">{topic.title}</h4>
-					<CardText className="d-flex mt-5">
-						<span
-							className="ml-auto"
-							style={{ position: 'relative', right: '50px' }}
-							onClick={() => props.upVoteHandle(topic.id)}
-						>
-							<i className="fas fa-2x fa-thumbs-up" style={{ color: 'green' }} /> {topic.upvote}
-						</span>
-						<span
-							className="mr-auto"
-							style={{ position: 'relative', left: '50px' }}
-							onClick={() => props.downVoteHandle(topic.id)}
-						>
-							<i className="fas fa-2x fa-thumbs-down" style={{ color: 'red' }} /> {topic.downvote}
-						</span>
-					</CardText>
-				</CardBody>
-			</Card>
-		</Col>
-	));
+	const topicList = props.topics.map(
+		(topic, i) =>
+			i < 20 && (
+				<Col className="my-3" lg={3} md={3} sm={6} key={topic.id}>
+					<Card style={{ height: '210px' }}>
+						<CardBody>
+							<h4 className="text-center">{topic.title}</h4>
+							<CardText className="d-flex mt-5">
+								<span
+									className="ml-auto"
+									style={{ position: 'relative', right: '50px' }}
+									onClick={() => props.upVoteHandle(topic.id)}
+								>
+									<i className="fas fa-2x fa-thumbs-up" style={{ color: 'green' }} /> {topic.upvote}
+								</span>
+								<span
+									className="mr-auto"
+									style={{ position: 'relative', left: '50px' }}
+									onClick={() => props.downVoteHandle(topic.id)}
+								>
+									<i className="fas fa-2x fa-thumbs-down" style={{ color: 'red' }} /> {topic.downvote}
+								</span>
+							</CardText>
+						</CardBody>
+					</Card>
+				</Col>
+			)
+	);
 
 	return (
 		<Row>
