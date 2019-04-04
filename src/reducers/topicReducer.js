ee89482ee9b87d5522477b59342725eb1d0f8f22
Anwar Abdullah <anwarabdullahn28@gmail.com>
2019-04-04 23:53:18 +0700

    add some redux setup

---
+++ src/reducers/topicReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+	topic: {},
+	topics: [],
+	msg: ''
+};
+
+export default function(state = initialState, action) {
+	switch (action.type) {
+		default:
+			return state;
+	}
+}
