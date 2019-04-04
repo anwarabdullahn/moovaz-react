258a62c19d00031f54c6792893014b8c746c1dc2
Anwar Abdullah <anwarabdullahn28@gmail.com>
2019-04-05 01:07:57 +0700

    add some basic style

--- src/components/Topic.js
+++ src/components/Topic.js
@@ -1,11 +1,53 @@
 import React from 'react';
+import { Card, CardText, CardBody, CardTitle, CardSubtitle, Button, Row, Col } from 'reactstrap';
+import { relative } from 'path';
 
 const Topic = (props) => {
 	console.log(props);
+
+	const addTopic = (
+		<Col className="my-3" lg={3} md={3} sm={6}>
+			<Card
+				style={{
+					borderStyle: 'dashed',
+					height: '210px',
+					backgroundColor: '#cecece',
+					color: '#fff',
+					borderWidth: '7px'
+				}}
+			>
+				<CardBody className="d-flex justify-content-center align-items-center">
+					<h3>
+						Add New Topic <i className="fas fa-plus" />
+					</h3>
+				</CardBody>
+			</Card>
+		</Col>
+	);
+
+	const topicList = props.topics.map((topic) => (
+		<Col className="my-3" lg={3} md={3} sm={6} key={topic.id}>
+			<Card style={{ height: '210px' }}>
+				<CardBody>
+					<h4 className="text-center">{topic.title}</h4>
+					<CardText className="d-flex mt-5">
+						<span className="ml-auto" style={{ position: 'relative', right: '50px' }}>
+							<i className="fas fa-2x fa-thumbs-up" style={{ color: 'green' }} /> {topic.upvote}
+						</span>
+						<span className="mr-auto" style={{ position: 'relative', left: '50px' }}>
+							<i className="fas fa-2x fa-thumbs-down" style={{ color: 'red' }} /> {topic.downvote}
+						</span>
+					</CardText>
+				</CardBody>
+			</Card>
+		</Col>
+	));
+
 	return (
-		<div>
-			<h1>Topic Components</h1>
-		</div>
+		<Row>
+			{topicList}
+			{addTopic}
+		</Row>
 	);
 };
 
--- src/containers/Landing.js
+++ src/containers/Landing.js
@@ -6,14 +6,14 @@ class Landing extends Component {
 		super();
 		this.state = {
 			topics: [
-				{ title: 'First Topic', upvote: 0, downvote: 0 },
-				{ title: 'Second Topic', upvote: 0, downvote: 0 }
+				{ id: 1111, title: 'First Topic', upvote: 0, downvote: 0 },
+				{ id: 2222, title: 'Second Topic', upvote: 0, downvote: 0 }
 			]
 		};
 	}
 	render() {
 		return (
-			<div>
+			<div className="mt-5 mx-3">
 				<Topic topics={this.state.topics} />
 			</div>
 		);
